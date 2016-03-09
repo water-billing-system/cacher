806723b73c4325a71269246645790a42b33c1e51
Serhat Perkmen <serhatperkmen@gmail.com>
2016-03-09 16:31:04 +0200

    implemented json array package

--- src/main/java/graduation/project/api/reader/App.java
+++ src/main/java/graduation/project/api/reader/App.java
@@ -4,42 +4,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.Charset;
 
+
+import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-	 private static String readAll(Reader rd) throws IOException {
-		    StringBuilder sb = new StringBuilder();
-		    int cp;
-		    while ((cp = rd.read()) != -1) {
-		      sb.append((char) cp);
-		    }
-		    return sb.toString();
-		  }
+		  
+		  private static String convertStreamToString(InputStream is) {
+		        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		        StringBuilder sb = new StringBuilder();
+		        String line = null;
+		        try {
+		            while ((line = reader.readLine()) != null) {
+		                sb.append(line).append("\n");
+		            }
+		        } catch (IOException e) {
+		        } finally {
+		            try {
+		                is.close();
+		            } catch (IOException e) {
+		            }
+		        }
 
-		  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-		    InputStream is = new URL(url).openStream();
-		    try {
-		      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-		      String jsonText = readAll(rd);
-		      JSONObject json = new JSONObject(jsonText);
-		      return json;
-		    } finally {
-		      is.close();
+		        return sb.toString();
 		    }
-		  }
 
 		  public static void main(String[] args) throws IOException, JSONException {
-		    JSONObject json = readJsonFromUrl("http://192.168.1.64:3000/bgs_tahaks/80712080");
-		    System.out.println(json.toString());
-		    System.out.println(json.get("NO"));
+		    
+			URL url1 = new URL("http://192.168.1.64:3000/bgs_tahaks");
+			HttpURLConnection request1 = (HttpURLConnection) url1.openConnection();
+	        request1.setRequestMethod("GET");
+	        request1.connect();
+	        String responseBody = convertStreamToString(request1.getInputStream());
+	        JSONArray jsonArray = new JSONArray(responseBody);
+	        
+	        for (int i = 0; i < jsonArray.length(); i++) {
+	            String email = jsonArray.getJSONObject(i).getString("NO");
+	            System.out.println(email);
+	        }
+			//JSONObject json = readJsonFromUrl("http://192.168.1.64:3000/bgs_tahaks/80712080");
+		    //System.out.println(json.toString());
+		    //System.out.println(json.get("NO"));
 		  }
 }
